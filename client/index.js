20f67716eb26c2f80f1a0602a4f219f87d3a0fe7
CouzDelCouzzz <quentin.diprima@gmail.com>
2023-03-01 14:50:44 +0100

    Adding comments

--- client/index.js
+++ client/index.js
@@ -9,7 +9,7 @@ async function main() {
     const merkleTree = new MerkleTree(niceList)
 
     // find the proof that the person is in the list
-    const name = "Chris Windler"
+    const name = "Chris Windler" // You can chose whatever you want. We can even make a website to enter a name
     const index = niceList.findIndex((n) => n === name)
     const proof = merkleTree.getProof(index)
 
